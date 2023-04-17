28c087b49ca26d2a89788ef2184ad9edf63b6cee
zinny912 <xbigbang5@gmail.com>
2023-04-17 15:29:01 +0900

    230417 init

---
+++ src/main/java/com/iu/base/Gdj60Boot1Application.java
@@ -0,0 +1,13 @@
+package com.iu.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdj60Boot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdj60Boot1Application.class, args);
+	}
+
+}
